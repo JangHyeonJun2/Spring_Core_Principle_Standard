04953db9080a4777dab2837a4e071b8f2279a17d
JangHyeonJun2 <epfvkdlxj@naver.com>
2020-12-13 15:01:01 +0900

    생성자 주입을 선택해라

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,6 +6,10 @@ import hello.core.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * 생성자에서 this.discountPolicy = discountPolicy; 이 구문이 없다면 컴파일 오류가 발생한다. 필드 값에서 이미 final이라고 선언을 했기 떄문인다.
+ * 수정자 주입을 포함한 나머지 주입 방식은 모두 생성자 이후에 호출되므로, 필드에 final 키워드를 사용할 수 없다. 오직 생성자 주입 방식만 final 키워드를 사용할 수 있다
+ */
 @Component
 public class OrderServiceImpl implements OrderService{
 
